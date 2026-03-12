e758d9a562ba5c764b19985546f5b16ecce900b6
okw <okw0204@gmail.com>
2026-03-12 17:08:43 +0900

    Tune editor input flow and transparency defaults

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,29 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local ime_group = vim.api.nvim_create_augroup("user_ime_control", { clear = true })
+
+-- fcitx5 を半角英数へ戻す（Insert以外は基本OFF運用）
+local function ime_off()
+  if vim.fn.executable("fcitx5-remote") ~= 1 then
+    return
+  end
+  pcall(vim.fn.system, { "fcitx5-remote", "-c" })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter", "InsertLeave", "CmdlineEnter" }, {
+  group = ime_group,
+  callback = ime_off,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = ime_group,
+  callback = function()
+    -- i/R/r 以外に遷移したらIMEをOFF
+    local mode = vim.fn.mode(1)
+    if not mode:match("^[iRr]") then
+      ime_off()
+    end
+  end,
+})
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- 日本語入力中でも素早くNormalへ戻れるようにする
+map("i", "jj", "<Esc>", { desc = "Insert -> Normal" })
+
+-- 削除系キーでレジスタ"(デフォルト)を汚さない
+-- 明示レジスタ指定("a など)がある場合は既存動作を優先する
+local function keep_register_or_blackhole(key)
+  return function()
+    if vim.v.register == '"' then
+      return '"_' .. key
+    end
+    return key
+  end
+end
+
+for _, key in ipairs({ "x", "X", "d", "D", "s", "S", "c", "C" }) do
+  map({ "n", "x" }, key, keep_register_or_blackhole(key), {
+    expr = true,
+    desc = "Delete/Change without yanking",
+  })
+end
