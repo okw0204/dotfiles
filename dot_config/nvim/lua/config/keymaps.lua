c121a0e367b18929e0fec31a341d416f7da4d590
okw <okw0204@gmail.com>
2026-03-23 20:53:52 +0900

    Update local keymap behavior and clarify AGENTS operations rules

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 -- 日本語入力中でも素早くNormalへ戻れるようにする
 map("i", "jj", "<Esc>", { desc = "Insert -> Normal" })
 
-for _, key in ipairs({ "x", "X", "d", "D", "s", "S", "c", "C" }) do
+for _, key in ipairs({ "x", "X", "d", "D", "c", "C" }) do
   map({ "n", "x" }, key, '"_' .. key, {
     desc = "Delete/Change without yanking",
   })
