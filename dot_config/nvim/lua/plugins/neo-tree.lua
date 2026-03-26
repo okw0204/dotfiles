69a9e27516018da8aa0c73fc2578ca9836adbecd
okw <okw0204@gmail.com>
2026-03-26 19:11:40 +0900

    Fix neo-tree y mapping clipboard behavior

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,19 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        mappings = {
+          -- y は Neo-tree 内部コピーではなく OS クリップボードへ絶対パスを入れる
+          ["y"] = function(state)
+            local node = state.tree:get_node()
+            if not node then
+              return
+            end
+            vim.fn.setreg("+", node:get_id(), "c")
+          end,
+          ["gy"] = "copy_to_clipboard",
+        },
+      },
       filesystem = {
         filtered_items = {
           visible = true,
