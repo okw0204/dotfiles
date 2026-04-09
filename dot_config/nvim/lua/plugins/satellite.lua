d0a9d6412320f3904c7da42fe78709011b42d098
okw <okw0204@gmail.com>
2026-04-09 14:02:30 +0900

    Add satellite.nvim scrollbar config

---
+++ dot_config/nvim/lua/plugins/satellite.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "lewis6991/satellite.nvim",
+    opts = {
+      handlers = {
+        cursor = { enable = true },
+        diagnostic = { enable = true },
+        gitsigns = { enable = true },
+        search = { enable = true },
+        marks = { enable = false },
+        quickfix = { enable = true },
+      },
+    },
+  },
+}
