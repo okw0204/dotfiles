d8c84572b49c083de710cbdb7ab50760347d4126
okw <okw0204@gmail.com>
2026-04-15 13:37:15 +0900

    Enable animated Neovim scrolling

---
+++ dot_config/nvim/lua/plugins/snacks-scroll.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = true,
+      animate = {
+        easing = "linear",
+        duration = {
+          step = 12,
+          total = 140,
+        },
+      },
+    },
+  },
+}
