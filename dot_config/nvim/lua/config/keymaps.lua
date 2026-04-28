02a35bfb8e9dae288cdb32a1391de12d938e412d
okw <okw0204@gmail.com>
2026-04-28 13:20:14 +0900

    Add wallpaper slideshow service

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -8,12 +8,6 @@ local map = vim.keymap.set
 map("i", "jj", "<Esc>", { desc = "Insert -> Normal" })
 map("i", "っｊ", "<Esc>", { desc = "Insert -> Normal (IME fallback)" })
 
-for _, key in ipairs({ "x", "X", "d", "D", "c", "C" }) do
-  map({ "n", "x" }, key, '"_' .. key, {
-    desc = "Delete/Change without yanking",
-  })
-end
-
 -- Rust のテスト候補実行を素早く呼べるようにする
 map("n", "<leader>r", "<nop>", { desc = "+rust" })
 map("n", "<leader>rt", function()
